f246274fc017dfa0d2c2f45adccd25d75384b759
jmieville <jm.mieville@gmail.com>
2017-03-29 02:53:56 +0700

    commit initial progress

---
+++ Sentiment Analysis/Sentiment Analysis/TextToSpeechVC.swift
@@ -0,0 +1,21 @@
+//
+//  TextToSpeechVC.swift
+//  Sentiment Analysis
+//
+//  Created by Jean-Marc Kampol Mieville on 3/29/2560 BE.
+//  Copyright © 2560 Jean-Marc Kampol Mieville. All rights reserved.
+//
+
+import UIKit
+
+class TextToSpeechVC: UIViewController {
+    @IBOutlet weak var textFieldLabel: UITextField!
+    @IBOutlet weak var speakButtonPressed: UIButton!
+
+    @IBAction func speakButtonPressed(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
