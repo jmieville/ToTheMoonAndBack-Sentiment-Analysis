36bf7101ae9350dbcd5890d5664030b13da63071
jmieville <jm.mieville@gmail.com>
2017-03-30 01:11:21 +0700

    Finished implementing the text to speech

--- Sentiment Analysis/Sentiment Analysis/TextToSpeechVC.swift
+++ Sentiment Analysis/Sentiment Analysis/TextToSpeechVC.swift
@@ -7,12 +7,42 @@
 //
 
 import UIKit
+import TextToSpeechV1
+import AVFoundation
 
 class TextToSpeechVC: UIViewController {
     @IBOutlet weak var textFieldLabel: UITextField!
     @IBOutlet weak var speakButtonPressed: UIButton!
+    
+    @IBOutlet weak var voiceSegment: UISegmentedControl!
+    
 
     @IBAction func speakButtonPressed(_ sender: Any) {
+        NSLog(textFieldLabel.text! + voiceSegment.selectedSegmentIndex.description)
+        
+        // Add watson service
+        let textToSpeech = TextToSpeech(username: userName, password: pwd)
+        
+        guard textFieldLabel.text != nil else {
+            NSLog("Error")
+            return
+        }
+        let text = textFieldLabel.text
+        let failure = { (error: Error) in print(error)
+        }
+        let voice = ("0" == voiceSegment.selectedSegmentIndex.description ? SynthesisVoice.us_Michael : ( "1" == voiceSegment.selectedSegmentIndex.description ? SynthesisVoice.us_Allison : SynthesisVoice.gb_Kate))
+        textToSpeech.synthesize(text!, voice: voice.rawValue, failure: failure) { data in
+            do {
+                var audioPlayer: AVAudioPlayer
+                audioPlayer = try! AVAudioPlayer(data: data)
+                audioPlayer.prepareToPlay()
+                audioPlayer.play()
+                sleep(10)
+            } catch {
+                NSLog("Something went terribly wrong")
+            }
+        }
+        
     }
     override func viewDidLoad() {
         super.viewDidLoad()
